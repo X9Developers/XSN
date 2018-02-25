3e6a29832dc5ffb3a2e5e20ffb930f6451d6ed78
Yura Oleksyshyn <yuraolex@gmail.com>
2018-02-25 12:39:05 +0200

    Updated spork testnet key

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -357,7 +357,7 @@ public:
 
         nPoolMaxTransactions = 3;
         nFulfilledRequestExpireTime = 5*60; // fulfilled requests expire in 5 minutes
-        strSporkPubKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
+        strSporkPubKey = "02a80a535f8adae9b557d5b1cf1e088333efdfd51c5e1af7f913483197e66433ab";
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
