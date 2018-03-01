4276ba3449fd483db6d3d8ab055eec19d40eb637
bwang22 <ttwanksta@gmail.com>
2018-02-28 22:29:18 -0500

    change getmasternodepayment

--- src/validation.cpp
+++ src/validation.cpp
@@ -1292,21 +1292,11 @@ CAmount GetBlockSubsidy(int nPrevHeight, const Consensus::Params& consensusParam
 
 CAmount GetMasternodePayment(int nHeight, CAmount blockValue)
 {
-    CAmount ret = blockValue/5; // start at 20%
-
-    //    int nMNPIBlock = Params().GetConsensus().nMasternodePaymentsIncreaseBlock;
-    //    int nMNPIPeriod = Params().GetConsensus().nMasternodePaymentsIncreasePeriod;
-
-    //    // mainnet:
-    //    if(nHeight > nMNPIBlock)                  ret += blockValue / 20; // 158000 - 25.0% - 2014-10-24
-    //    if(nHeight > nMNPIBlock+(nMNPIPeriod* 1)) ret += blockValue / 20; // 175280 - 30.0% - 2014-11-25
-    //    if(nHeight > nMNPIBlock+(nMNPIPeriod* 2)) ret += blockValue / 20; // 192560 - 35.0% - 2014-12-26
-    //    if(nHeight > nMNPIBlock+(nMNPIPeriod* 3)) ret += blockValue / 40; // 209840 - 37.5% - 2015-01-26
-    //    if(nHeight > nMNPIBlock+(nMNPIPeriod* 4)) ret += blockValue / 40; // 227120 - 40.0% - 2015-02-27
-    //    if(nHeight > nMNPIBlock+(nMNPIPeriod* 5)) ret += blockValue / 40; // 244400 - 42.5% - 2015-03-30
-    //    if(nHeight > nMNPIBlock+(nMNPIPeriod* 6)) ret += blockValue / 40; // 261680 - 45.0% - 2015-05-01
-    //    if(nHeight > nMNPIBlock+(nMNPIPeriod* 7)) ret += blockValue / 40; // 278960 - 47.5% - 2015-06-01
-    //    if(nHeight > nMNPIBlock+(nMNPIPeriod* 9)) ret += blockValue / 40; // 313520 - 50.0% - 2015-08-03
+    int treasuryPercent = 10;
+    CAmount moneySupply = chainActive.Tip()->nMoneySupply;
+    int nMasternodeCount = mnodeman.size();
+    int64_t mNodeCoins = nMasternodeCount * 10000 * COIN;
+    CAmount ret = blockValue * ((moneySupply - mNodeCoins) / moneySupply) * ((100 - treasuryPercent)/100);
 
     return ret;
 }
@@ -4083,7 +4073,7 @@ bool LoadBlockIndex()
     return true;
 }
 
-bool InitBlockIndex(const CChainParams& chainparams) 
+bool InitBlockIndex(const CChainParams& chainparams)
 {
     LOCK(cs_main);
 
