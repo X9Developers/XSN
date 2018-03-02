a5af1d2887290d8803df0c7d3c895b3b1b3c12be
Yura Oleksyshyn <yuraolex@gmail.com>
2018-03-02 19:48:50 +0200

    Updated to DGW

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -138,7 +138,7 @@ public:
         consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.fPowNoRetargeting = false;
         consensus.nPowKGWHeight = 15200;
-        consensus.nPowDGWHeight = 34140;
+        consensus.nPowDGWHeight = 24;
         consensus.nLastPoWBlock = 100;
         consensus.nRuleChangeActivationThreshold = 1916; // 95% of 2016
         consensus.nMinerConfirmationWindow = 2016; // nPowTargetTimespan / nPowTargetSpacing
