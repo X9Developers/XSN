834efbc2c89c4ade91ea148d0b12e0fd1bede45f
Yura Oleksyshyn <yuraolex@gmail.com>
2018-03-05 00:39:01 +0200

    Updated merchantnode params

--- src/tpos/merchantnode.h
+++ src/tpos/merchantnode.h
@@ -16,8 +16,8 @@ class CConnman;
 
 static const int MERCHANTNODE_CHECK_SECONDS               = 20;
 static const int MERCHANTNODE_MIN_MNB_SECONDS             = 5 * 60;
-static const int MERCHANTNODE_MIN_MNP_SECONDS             = 60 * 60;
-static const int MERCHANTNODE_EXPIRATION_SECONDS          = 150 * 60;
+static const int MERCHANTNODE_MIN_MNP_SECONDS             = 10 * 60;
+static const int MERCHANTNODE_EXPIRATION_SECONDS          = 65 * 60;
 static const int MERCHANTNODE_MAX_EXPIRATION_SECONDS      = 25 * 60 * 60;
 static const int MERCHANTNODE_WATCHDOG_MAX_SECONDS        = 120 * 60;
 static const int MERCHANTNODE_NEW_START_REQUIRED_SECONDS  = 180 * 60;
@@ -214,9 +214,12 @@ public:
 
     bool IsValidForPayment() const
     {
-        if(nActiveState == MERCHANTNODE_ENABLED) {
+
+        if(nActiveState == MERCHANTNODE_ENABLED && IsPoSeVerified()) {
             return true;
         }
+
+
 
 //        if(!sporkManager.IsSporkActive(SPORK_14_REQUIRE_SENTINEL_FLAG) &&
 //           (nActiveState == MERCHANTNODE_WATCHDOG_EXPIRED)) {
