504558c7c4f8d4082265b347656f156523756912
AndriiDiachuk <andriy.dyachuk95@gmail.com>
2018-04-06 14:17:10 +0300

    Fixed bug with unexpectly closing Qt wallet in transaction tab

--- src/qt/transactionview.cpp
+++ src/qt/transactionview.cpp
@@ -407,6 +407,9 @@ void TransactionView::contextualMenu(const QPoint &point)
     QModelIndex index = transactionView->indexAt(point);
     QModelIndexList selection = transactionView->selectionModel()->selectedRows(0);
 
+    if(selection.isEmpty())
+        return;
+
     // check if transaction can be abandoned, disable context menu action in case it doesn't
     uint256 hash;
     hash.SetHex(selection.at(0).data(TransactionTableModel::TxHashRole).toString().toStdString());
