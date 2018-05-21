3c3db60dd71b8eccea3759f774dc6abba0f43f34
Carlos Melo <carlosmmelo@gmail.com>
2018-05-21 11:16:48 -0400

    WAL-4: fixing tesnet class

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -227,7 +227,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1493596800; // May 1st 2017
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x00000000000000000000000000000000000000000000002830dab7f76dbb7d63");
+        consensus.nMinimumChainWork = uint256S("0x0");
 
         // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S("0x0000000002e9e7b00e1f6dc5123a04aad68dd0f0968d8c7aa45f6640795c37b1"); //1135275
