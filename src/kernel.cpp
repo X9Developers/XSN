2d2f3bb623ca70dc8258063ef2cdc93d6f8c34ab
Yura Oleksyshyn <yuraolex@gmail.com>
2018-06-08 13:00:27 +0300

    Updated testnet staking algo

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -265,7 +265,18 @@ static bool GetKernlStakeModifierV03(uint256 hashBlockFrom, unsigned int nTimeTx
     while (nStakeModifierTime < pindexFrom->GetBlockTime() + nStakeModifierSelectionInterval) {
         if (!pindexNext) {
             // Should never happen
-            return error("Null pindexNext\n");
+            if(Params().NetworkIDString() == CBaseChainParams::TESTNET)
+            {
+                nStakeModifierHeight = pindexFrom->nHeight;
+                nStakeModifierTime = pindexFrom->GetBlockTime();
+                if(pindex->GeneratedStakeModifier())
+                    nStakeModifier = pindex->nStakeModifier;
+                return true;
+            }
+            else
+            {
+                return error("Null pindexNext\n");
+            }
         }
 
         pindex = pindexNext;
