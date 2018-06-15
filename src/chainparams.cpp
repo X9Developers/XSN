52fec7e321fce3b0f547b3c6b34a93b76f9cb7fc
Carlos Melo <carlosmmelo@gmail.com>
2018-06-15 16:57:52 -0400

    WAL-30: Adding xsn seeder domain

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -158,6 +158,7 @@ public:
 //        vSeeds.emplace_back("seed.xsn.jonasschnelli.ch"); // Jonas Schnelli, only supports x1, x5, x9, and xd
 //        vSeeds.emplace_back("seed.xsn.petertodd.org"); // Peter Todd, only supports x1, x5, x9, and xd
 //        vSeeds.emplace_back("seed.xsn.sprovoost.nl"); // Sjors Provoost
+        vSeeds.emplace_back("autoseeds.xsnseed.xyz");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,76);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,16);
