2a96a282f1ddb0488e52631cce9b72ff8af8d190
Yura Oleksyshyn <yuraolex@gmail.com>
2018-07-13 17:48:37 +0300

    Updated locking requirment for SyncTransaction

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -738,7 +738,7 @@ private:
 
     /* Used by TransactionAddedToMemorypool/BlockConnected/Disconnected.
      * Should be called with pindexBlock and posInBlock if this is for a transaction that is included in a block. */
-    void SyncTransaction(const CTransactionRef& tx, const CBlockIndex *pindex = nullptr, int posInBlock = 0);
+    void SyncTransaction(const CTransactionRef& tx, const CBlockIndex *pindex = nullptr, int posInBlock = 0) EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
 
     /* the HD chain data model (external chain counters) */
     CHDChain hdChain;
