12b998632e0f83b38f3a5accee913d5cddf015ea
Yura Oleksyshyn <yuraolex@gmail.com>
2018-07-16 19:01:57 +0300

    Changed deployment time for segwit on testnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -251,7 +251,7 @@ public:
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1528748400; // June 11 2018
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1531764000; // June 11 2018
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1560284399; // June 11 2019
 
         // Deployment of BIP68, BIP112, and BIP113.
