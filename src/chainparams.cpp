19f7c4f78c89ae2c19b2cfec59f42920a7d3f5c8
Yura Oleksyshyn <yuraolex@gmail.com>
2018-07-17 10:19:43 +0300

    Fixed resource path for some icons

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,7 +120,7 @@ public:
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1532995140; // July 24th, 2018.
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1532995140; // July 30th, 2018.
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1564012740; // July 24th, 2019.
 
         // The best chain should have at least this much work.
