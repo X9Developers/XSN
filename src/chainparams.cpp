86f3a4666a7bca9e56e4aa39a99367d337a4f89e
Yura Oleksyshyn <yuraolex@gmail.com>
2018-07-30 20:38:45 +0300

    Updated genesis for testnet, changed segwit deployment time

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -9,6 +9,7 @@
 #include <tinyformat.h>
 #include <util.h>
 #include <utilstrencodings.h>
+#include <arith_uint256.h>
 
 #include <assert.h>
 
@@ -251,7 +252,7 @@ public:
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1532811600; // June 11 2018
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1532984340; // June 30 2018
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1560284399; // June 11 2019
 
         // Deployment of BIP68, BIP112, and BIP113.
@@ -277,9 +278,10 @@ public:
         nDefaultPort = 29999;
         nPruneAfterHeight = 1000;
 
-        genesis = CreateGenesisBlock(1516111682, 2411518, 0x1e0ffff0, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1532971893, 1866723, 0x1e0ffff0, 1, 50 * COIN);
+
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000005254b31cd4e41fdce02c5a571fe4b6ec6d65af0dc339a85bb749ee90763"));
+        assert(consensus.hashGenesisBlock == uint256S("000004a4831f2745513d90ddc038bd1657696981d4baff675d8760ca44cb2f73"));
         assert(genesis.hashMerkleRoot == uint256S("0x922ab2360f766457416dfc59c6594248c5b79e33c8785bce491c0e01930738f6"));
 
         vFixedSeeds.clear();
@@ -311,7 +313,7 @@ public:
 
         checkpointData = {
             {
-                {0, uint256S("0x000005254b31cd4e41fdce02c5a571fe4b6ec6d65af0dc339a85bb749ee90763")},
+                {0, uint256S("000004a4831f2745513d90ddc038bd1657696981d4baff675d8760ca44cb2f73")},
             }
         };
 
