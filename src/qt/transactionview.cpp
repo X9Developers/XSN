3e396ceaf79bd3e7ebcd0fdb3448292c7f2b33ce
AndriiDiachuk <andriy.dyachuk95@gmail.com>
2018-08-20 11:49:05 +0300

    Bugfix/103 (#105)
    * Fixed tposcontract cleanup issue, fixed typo mistakes in UI
    
    * Fixed issue with filtering MintedTPoS, fixed issue with sendtoadress with amount of splits

--- src/qt/transactionview.cpp
+++ src/qt/transactionview.cpp
@@ -91,6 +91,7 @@ TransactionView::TransactionView(const PlatformStyle *platformStyle, QWidget *pa
     typeWidget->addItem(tr("To yourself"), TransactionFilterProxy::TYPE(TransactionRecord::SendToSelf));
     typeWidget->addItem(tr("Mined"), TransactionFilterProxy::TYPE(TransactionRecord::Generated));
     typeWidget->addItem(tr("Minted"), TransactionFilterProxy::TYPE(TransactionRecord::StakeMint));
+    typeWidget->addItem(tr("Minted(TPoS)"), TransactionFilterProxy::TYPE(TransactionRecord::StakeMintTPoS));
     typeWidget->addItem(tr("Masternode Reward"), TransactionFilterProxy::TYPE(TransactionRecord::MNReward));
     typeWidget->addItem(tr("Other"), TransactionFilterProxy::TYPE(TransactionRecord::Other));
 
