d96b9fcb22e47bb48b778c5c242635eea1c9bcc8
Yura <yuraolex@gmail.com>
2020-08-25 14:47:56 +0300

    Update checkblock.cpp

--- src/bench/checkblock.cpp
+++ src/bench/checkblock.cpp
@@ -48,7 +48,7 @@ static void DeserializeAndCheckBlockTest(benchmark::State& state)
         assert(stream.Rewind(sizeof(block_bench::block413567)));
 
         CValidationState validationState;
-        assert(CheckBlock(block, validationState, chainParams->GetConsensus(), 0));
+        assert(CheckBlock(block, validationState, chainParams->GetConsensus()));
     }
 }
 
